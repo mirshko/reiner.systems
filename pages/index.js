127832b66764090b5078074f9764221aee423e92
Jeff Reiner <theundeadghost@gmail.com>
2020-01-28 21:18:25 +0100

    :sparkles: Merge about into home

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,40 @@ export default function Page() {
   return (
     <main>
       <p>
-        heya. my name is jeff reiner: i'm a freelance ux engineer based in
-        berlin.
-      </p>
-
-      <p>
-        i focus on responsive, mobile-first websites and apps; highly-usable and
+        I focus on responsive, mobile-first websites and apps; highly-usable and
         efficient component libraries and design systems; and solving complex UX
         problems, in and around the web2 and web3 space.
       </p>
+      <ul>
+        <li>
+          <p>Freelance UX Engineer.</p>
+        </li>
+        <li>
+          <p>
+            Team at{" "}
+            <a
+              href="https://www.instagram.com/we_are_coal/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Coal
+            </a>
+          </p>
+        </li>
+      </ul>
+
+      <p>
+        here are some places where i post things. twitter is me tweeting about
+        dev, design, blockchain stuff: hmu there. for those wanting one of those
+        more official things: a <a href="/jeff-reiner.pdf">résumé</a> for you.
+      </p>
+
+      <ul>
+        <li>[github](https://github.com/mirshko)</li>
+        <li>[instagram](https://www.instagram.com/mirshko)</li>
+        <li>[twitter](https://twitter.com/mirshko)</li>
+        <li>[keybase](https://keybase.io/mirshko)</li>
+      </ul>
     </main>
   );
 }
