55ca86971d69e7a114800443d33ecc8fb95b78c5
Jeff Reiner <theundeadghost@gmail.com>
2020-02-12 00:17:57 +0100

    :mag: Adding beginnings of SEO

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,40 @@
+import Head from "next/head";
+
 export default function Page() {
   return (
     <main>
-      <p>
+      <Head>
+        <title>Jeff Reiner, UX Engineer</title>
+      </Head>
+
+      <p className="measure">
+        UX Engineer. Team at{" "}
+        <a
+          href="https://www.instagram.com/we_are_coal/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Coal
+        </a>
+      </p>
+
+      <p className="measure">
         I focus on responsive, mobile-first websites and apps; highly-usable and
         efficient component libraries and design systems; and solving complex UX
-        problems, in and around the web2 and web3 space.
-      </p>
-      <ul>
-        <li>
-          <p>Freelance UX Engineer.</p>
-        </li>
-        <li>
-          <p>
-            Team at{" "}
-            <a
-              href="https://www.instagram.com/we_are_coal/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Coal
-            </a>
-          </p>
-        </li>
-      </ul>
-
-      <p>
-        here are some places where i post things. twitter is me tweeting about
-        dev, design, blockchain stuff: hmu there. for those wanting one of those
-        more official things: a <a href="/jeff-reiner.pdf">résumé</a> for you.
+        problems, in and around the Web 2.0 and Web3 space.
       </p>
 
-      <ul>
-        <li>[github](https://github.com/mirshko)</li>
-        <li>[instagram](https://www.instagram.com/mirshko)</li>
-        <li>[twitter](https://twitter.com/mirshko)</li>
-        <li>[keybase](https://keybase.io/mirshko)</li>
-      </ul>
+      <p className="measure">
+        <a href="/jeff-reiner.pdf" rel="noopener noreferrer" target="_blank">
+          Resume
+        </a>
+      </p>
+
+      <style jsx>{`
+        .measure {
+          max-width: 60ch;
+        }
+      `}</style>
     </main>
   );
 }
