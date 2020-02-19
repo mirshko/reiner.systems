37a7e5a26fbca620cc572855a75506ef3f6deab0
Jeff Reiner <theundeadghost@gmail.com>
2020-02-19 23:57:38 +0100

    :lipstick: Updating discogs design

--- components/discogs.js
+++ components/discogs.js
@@ -6,22 +6,39 @@ const Record = ({ basic_information: { artists, title, cover_image } }) => {
 
   return (
     <div>
-      <img
-        src={cover_image}
-        loading="lazy"
-        height={300}
-        width={300}
-        alt={label}
-      />
+      <div className="cover">
+        <img
+          src={cover_image}
+          loading="lazy"
+          height={300}
+          width={300}
+          alt={label}
+        />
+      </div>
 
-      <p>{label}</p>
+      <p title={label}>{label}</p>
 
       <style jsx>{`
+        .cover {
+          box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.5);
+          border-radius: 3px;
+          overflow: hidden;
+          position: relative;
+        }
+
         img {
           display: block;
           height: 300px;
           width: 300px;
           object-fit: cover;
+        }
+
+        p {
+          margin-top: 5px;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          line-height: 25px;
+          overflow: hidden;
         }
       `}</style>
     </div>
@@ -113,7 +130,8 @@ const Discogs = () => {
         section {
           display: grid;
           grid-template-columns: repeat(auto-fill, 300px);
-          max-width: 1200px;
+          grid-column-gap: 50px;
+          grid-row-gap: 20px;
         }
       `}</style>
     </section>
