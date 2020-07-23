08b2e398567acdf80c02519d6d38dc90c6bdadcd
Jeff Reiner <gone@hey.com>
2020-07-23 10:01:18 +0200

    Remove author, it me

--- components/post-details.js
+++ components/post-details.js
@@ -1,10 +1,10 @@
 const { Fragment } = require("react");
 
-const PostDetails = ({ title, author, dateTime, date, topic }) => (
+const PostDetails = ({ title, dateTime, date, topic }) => (
   <Fragment>
     <h1 className="mb-md">{title}</h1>
     <p className="mb-lg">
-      {author}, <time dateTime={dateTime}>{date}</time>, {topic}
+      <time dateTime={dateTime}>{date}</time>, {topic}
     </p>
   </Fragment>
 );
