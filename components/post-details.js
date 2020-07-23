af540460ee4597a36737a0846f6f6e21f16772f2
Jeff Reiner <gone@hey.com>
2020-07-23 09:55:22 +0200

    Adding PostDetails component

---
+++ components/post-details.js
@@ -0,0 +1,12 @@
+const { Fragment } = require("react");
+
+const PostDetails = ({ title, author, dateTime, date, topic }) => (
+  <Fragment>
+    <h1 className="mb-md">{title}</h1>
+    <p className="mb-lg">
+      {author}, <time dateTime={dateTime}>{date}</time>, {topic}
+    </p>
+  </Fragment>
+);
+
+export default PostDetails;
