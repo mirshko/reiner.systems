b22ceb656654b7eaaaf478dde7f6edf85a675239
Jeff Reiner <gone@hey.com>
2020-07-27 19:12:35 +0200

    Upgrade next

--- next.config.js
+++ next.config.js
@@ -8,4 +8,29 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: "/projects",
+        destination: "/portfolio",
+        permanent: true,
+      },
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "https://twitter.com/mirshko",
+        permanent: false,
+      },
+      {
+        source: "/photos",
+        destination: "https://www.are.na/jeff-reiner/index",
+        permanent: false,
+      },
+    ];
+  },
 });
