a6102399f84cc95cb9b4a45f0096315b71ca34f6
Jeff Reiner <gone@hey.com>
2020-08-09 17:06:19 +0200

    Some post details cleanup

--- components/post-details.js
+++ components/post-details.js
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
 
-const PostDetails = ({ title, published, topic }) => (
-  <Fragment>
-    <h1 className="mb-md">{title}</h1>
-    <p className="mb-lg">
-      {published && <time dateTime={published}>{published}</time>}
-      {topic && (published ? `, ${topic}` : topic)}
-    </p>
-  </Fragment>
-);
+const PostDetails = ({ title, published, topic }) => {
+  return (
+    <Fragment>
+      <h1 className="mb-md">{title}</h1>
+      <p className="mb-lg">
+        {published && <time dateTime={published}>{published}</time>}
+        {topic && (published ? `, ${topic}` : topic)}
+      </p>
+    </Fragment>
+  );
+};
 
 export default PostDetails;
