8378405c27052a728e8063deba7115be58a334e5
Jeff Reiner <gone@hey.com>
2020-08-17 21:35:26 +0200

    Adding leading to p tags

--- components/mdx-wrapper.js
+++ components/mdx-wrapper.js
@@ -7,7 +7,7 @@ import PostWrapper from "./post-wrapper";
 const components = {
   wrapper: (props) => <PostWrapper {...props} />,
   h1: (props) => <h1 className="mb-8" {...props} />,
-  p: (props) => <p className="mb-4 max-w-xl" {...props} />,
+  p: (props) => <p className="mb-4 max-w-xl leading-tight" {...props} />,
 };
 
 const MDXWrapper = ({ children }) => (
