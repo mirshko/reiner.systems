0ac88958a7b7d2ad1ff93974cf3e6f33f95643a6
Jeff Reiner <gone@hey.com>
2020-10-24 16:13:18 +0200

    Fix iphone bottom safe area, add bigger touch targets

--- components/nav.js
+++ components/nav.js
@@ -8,7 +8,10 @@ const NavLink = ({ href, children }) => {
 
   const isActive = pathname === href;
 
-  const cachedClassNames = classNames(isActive ? "text-primary" : "text-gray");
+  const cachedClassNames = classNames(
+    "p-2",
+    isActive ? "text-primary" : "text-gray"
+  );
 
   return (
     <Link href={href}>
@@ -19,8 +22,8 @@ const NavLink = ({ href, children }) => {
 
 const Nav = () => {
   return (
-    <nav className="cutout-bottom bg-black px-5 py-3  fixed inset-x-0 bottom-0 z-50">
-      <div className="max-w-xl mx-auto flex justify-between">
+    <nav className="cutout-bottom bg-black fixed inset-x-0 bottom-0 z-50">
+      <div className="px-3 py-1 max-w-xl mx-auto flex justify-between">
         <NavLink href="/">
           <Home />
         </NavLink>
