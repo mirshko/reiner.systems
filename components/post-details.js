9f471b6c539abd9cd9ec3df1ea98c4cd05d997de
Jeff Reiner <gone@hey.com>
2020-10-25 11:54:35 +0100

    Removing margin

--- components/post-details.js
+++ components/post-details.js
@@ -2,8 +2,8 @@ import { Fragment } from "react";
 
 const PostDetails = ({ title, published, topic }) => (
   <Fragment>
-    <h1 className="mb-4">{title}</h1>
-    <p className="mb-8">
+    <h1>{title}</h1>
+    <p>
       {published && <time dateTime={published}>{published}</time>}
       {topic && (published ? `, ${topic}` : topic)}
     </p>
