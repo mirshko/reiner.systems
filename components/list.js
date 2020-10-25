0f8ab3562e9ea173d2b7c36ec6d92071ef280080
Jeff Reiner <gone@hey.com>
2020-10-25 12:20:31 +0100

    Fix alignment of list

--- components/list.js
+++ components/list.js
@@ -1,33 +1,6 @@
-import Link from "next/link";
-import PropTypes from "prop-types";
-
-/**
- * @typedef Link
- * @description Object with the label and href for the linked work / project / etc...
- * @property {String} label
- * @property {String} href
- * @property {Boolean} isInternal
- * @property {String} screenshot
- *
- * @name List
- * @description Component to render a list of external links
- * @param {Array<Link>} data
- */
 const List = ({ data }) => (
   <ul>
-    {data.map(({ href, isInternal, label, screenshot }, i) => {
-      if (isInternal)
-        return (
-          <li key={i}>
-            <Link href={href}>
-              <a>
-                <span className="label">{label}</span>
-              </a>
-            </Link>{" "}
-            &nbsp;
-          </li>
-        );
-
+    {data.map(({ href, label, screenshot }, i) => {
       return (
         <li key={i} className="flex sm:inline-flex sm:mr-4">
           <a className="flex items-center" href={href}>
@@ -39,7 +12,7 @@ const List = ({ data }) => (
               loading="lazy"
               src={`/experiments/${screenshot}`}
             />
-            <span className="flex-1 -mt-1 leading-none">{label}</span>
+            <span className="flex-1 leading-none">{label}</span>
           </a>
           &nbsp;
         </li>
@@ -47,19 +20,5 @@ const List = ({ data }) => (
     })}
   </ul>
 );
-
-List.propTypes = {
-  /**
-   * Array of data to render as links
-   */
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      href: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      isInternal: PropTypes.bool,
-      screenshot: PropTypes.string,
-    }).isRequired
-  ).isRequired,
-};
 
 export default List;
