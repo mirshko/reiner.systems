15b628a5f76dc2a10c01664b51080853da61ff43
Jeff Reiner <gone@hey.com>
2020-11-03 16:28:06 +0100

    Remove YouTube from sync function

--- pages/api/sync/discogs-to-fauna.js
+++ pages/api/sync/discogs-to-fauna.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-console */
 
-import { createRecordsInFauna, getRecordsInFauna } from "../../../lib/fauna";
 import { getRecordsInDiscogs } from "../../../lib/discogs";
-import { getYouTubeVideoId } from "../../../lib/youtube";
+import { createRecordsInFauna, getRecordsInFauna } from "../../../lib/fauna";
 
 export default async (req, res) => {
   try {
@@ -26,37 +25,7 @@ export default async (req, res) => {
     if (recordsInFauna.length < recordsInDiscogs.length) {
       console.log("Fauna & Discogs Collections Are Out Of Sync! Updating!");
 
-      await createRecordsInFauna(recordsInDiscogs);
-    }
-
-    /**
-     * Get videos in Fauna that do not have a valid 'video_id'
-     */
-    const recordsWithoutVideos = recordsInFauna.filter(
-      (record) => record?.video_id === "null"
-    );
-
-    console.log("Number Of Records w/o Videos:", recordsWithoutVideos.length);
-
-    /**
-     * Attempt to populate records without videos with a YouTube video
-     */
-    const recordsToUpdate = await Promise.all(
-      recordsWithoutVideos.map(getYouTubeVideoId)
-    );
-
-    /**
-     * Filter out 'undefined' results from 'getYouTubeVideoId' call
-     */
-    const validRecordsToUpdate = recordsToUpdate.filter(Boolean);
-
-    console.log("Number Of Records To Update:", validRecordsToUpdate.length);
-
-    if (validRecordsToUpdate.length > 0) {
-      /**
-       * Update the records in Fauna with new updated 'video_id' property
-       */
-      const refs = await createRecordsInFauna(validRecordsToUpdate);
+      const refs = await createRecordsInFauna(recordsInDiscogs);
 
       res.status(200).json({
         success: true,
