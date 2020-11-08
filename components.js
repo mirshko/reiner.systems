b5596b8493e1a782fdd3969264869498b530dacd
Jeff Reiner <gone@hey.com>
2020-11-08 22:03:03 +0100

    404 to MDX

--- components.js
+++ components.js
@@ -1,21 +1,25 @@
-import Link from "next/link";
+import InternalLink from "next/link";
 import Page from "./components/page";
+
+const Link = (props) => {
+  const isExternal = props?.href && props?.href.startsWith("http");
+
+  if (isExternal)
+    return <a {...props} rel="noreferrer noopener" target="_blank" />;
+
+  return (
+    <InternalLink href={props.href}>
+      <a>{props.children}</a>
+    </InternalLink>
+  );
+};
 
 /**
  * @type {import("@mdx-js/react").MDXProviderComponentsProp}
  */
-export default {
+const components = {
   wrapper: (props) => <Page {...props} />,
-  a: (props) => {
-    const isExternal = props?.href && props?.href.startsWith("http");
-
-    if (isExternal)
-      return <a {...props} rel="noreferrer noopener" target="_blank" />;
-
-    return (
-      <Link href={props.href}>
-        <a>{props.children}</a>
-      </Link>
-    );
-  },
+  a: Link,
 };
+
+export default components;
