bc034bc07a43a5eed461f22a3a245299959fff7a
Jeff Reiner <theundeadghost@gmail.com>
2021-10-22 17:37:03 +0100

    Adding in first snippet

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,15 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import {
+  defineDocumentType,
+  makeSource,
+  ComputedFields,
+} from "contentlayer/source-files";
+
+const computedFields: ComputedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+  },
+};
 
 const Snippet = defineDocumentType(() => ({
   name: "Snippet",
@@ -8,6 +19,7 @@ const Snippet = defineDocumentType(() => ({
     title: { type: "string", required: true },
     description: { type: "string", required: true },
   },
+  computedFields,
 }));
 
 const contentLayerConfig = makeSource({
