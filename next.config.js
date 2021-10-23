a9e1e5f1bb60435ef5febda934bb3895bb7f633d
Jeff Reiner <theundeadghost@gmail.com>
2021-10-23 14:49:38 +0100

    Fix?

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const { withContentlayer } = require("next-contentlayer");
  * @type {import('next').NextConfig}
  **/
 module.exports = withContentlayer()({
+  experimental: {
+    esmExternals: true,
+    swcLoader: true,
+  },
+  reactStrictMode: true,
   trailingSlash: false,
   redirects() {
     return [
