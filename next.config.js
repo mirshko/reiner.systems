8d855ff9077ec50076ad505cdcc7f332b8e6d2d4
Jeff Reiner <theundeadghost@gmail.com>
2021-10-23 14:33:51 +0100

    Fix

--- next.config.js
+++ next.config.js
@@ -5,33 +5,4 @@ const { withContentlayer } = require("next-contentlayer");
  **/
 module.exports = withContentlayer()({
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: "/projects",
-        destination: "/portfolio",
-        permanent: true,
-      },
-      {
-        source: "/about",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/contact",
-        destination: "https://twitter.com/mirshko",
-        permanent: false,
-      },
-      {
-        source: "/photos",
-        destination: "https://www.are.na/jeff-reiner/index",
-        permanent: false,
-      },
-      {
-        source: "/experiments",
-        destination: "https://github.com/mirshko",
-        permanent: false,
-      },
-    ];
-  },
 });
