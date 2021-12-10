96fea5ba8f45b058c8ad27c4983887976390d6f3
Jeff Reiner <theundeadghost@gmail.com>
2021-12-10 19:00:07 +0100

    Remove and upgrade shit

--- components/nav.tsx
+++ components/nav.tsx
@@ -13,10 +13,6 @@ function Nav() {
         <NavLink href="/vinyl" activeClassName="text-indigo-dark">
           Vinyl
         </NavLink>
-
-        <NavLink href="/snippets" activeClassName="text-teal-dark">
-          Snippets
-        </NavLink>
       </div>
     </nav>
   );
