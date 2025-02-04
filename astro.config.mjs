84cec8af04556c4095633ba5918377b5cf2a4c2e
Jeff Reiner <theundeadghost@gmail.com>
2025-02-04 19:10:13 +0100

    Use native redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,15 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/about": "/",
+    "/info": "/",
+    "/projects": "/",
+    "/contact": "/",
+    "/photos": "/",
+    "/experiments": "/",
+    "/portfolio": "/",
+  },
   integrations: [tailwind({ applyBaseStyles: false }), react()],
   markdown: {
     shikiConfig: {
